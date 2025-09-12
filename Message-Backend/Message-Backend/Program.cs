47352af604fa2b9c5e41fad8c41ff6784049da80
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-12 21:31:26 +0200

    configure Program.cs

--- Message-Backend/Message-Backend/Program.cs
+++ Message-Backend/Message-Backend/Program.cs
@@ -1,3 +1,6 @@
+using Message_Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContextPool<MessageContext>(opt=>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("MessageDb")));
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var messageContext = scope.ServiceProvider.GetRequiredService<MessageContext>();
+    messageContext.Database.EnsureCreated();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
