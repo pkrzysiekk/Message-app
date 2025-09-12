a22f9f71e6464429b0fe36cae65557bc79df0eb0
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-12 21:30:49 +0200

    Implement GroupRole enum

--- Message-Backend/Message-Backend/Models/UserGroup.cs
+++ Message-Backend/Message-Backend/Models/UserGroup.cs
@@ -1,10 +1,12 @@
+using Message_Backend.Models.Enums;
+
 namespace Message_Backend.Models;
 
 public class UserGroup
 {
     public int UserId { get; set; }
     public int GroupId { get; set; }
-    public Enum Role { get; set; }
+    public GroupRole Role { get; set; }
     
     public User User { get; set; }
     public Group Group { get; set; }
