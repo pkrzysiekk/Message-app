8a121d654c60fdca1fe2a433fbcd6aaed860f8b2
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-14 20:19:09 +0200

    Refactor UserRepository.cs for idenity

--- Message-Backend/Message-Backend/Repository/UserRepository.cs
+++ Message-Backend/Message-Backend/Repository/UserRepository.cs
@@ -1,31 +1,31 @@
 using Message_Backend.Data;
 using Message_Backend.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Message_Backend.Repository;
 
-public class UserRepository : IRepository<User>
+public class UserRepository : IUserRepository
 {
-    private readonly MessageContext _context;
+    private readonly UserManager<User> _userManager;
 
-    public UserRepository(MessageContext context)
+    public UserRepository(UserManager<User> userManager)
     {
-       _context = context; 
+        _userManager = userManager;
     }
     
-    public async Task Create(User item)
+    public async Task Create(User item,string password)
     {
-        _context.Users.Add(item);
-        await SaveChanges();
+        await _userManager.CreateAsync(item,password);
     }
 
     public IQueryable<User> GetAll()
     {
-        return _context.Users.AsQueryable();
+        return _userManager.Users.AsQueryable();
     }
 
     public async Task<User?> GetById(int id)
     {
-        return await _context.Users.FindAsync(id);
+        return await _userManager.FindByIdAsync(id.ToString());
     }
 
     public async Task Update(User user)
@@ -33,8 +33,10 @@ public class UserRepository : IRepository<User>
         var userToUpdate = await GetById(user.Id);
         if (userToUpdate == null)
             throw new Exception("User not found");
-        userToUpdate.Username = user.Username;
-        await SaveChanges();
+        userToUpdate.UserName = user.UserName;
+        userToUpdate.Email = user.Email;
+        await _userManager.UpdateAsync(userToUpdate);
+        
     }
 
     public async Task Delete(int id)
@@ -42,13 +44,13 @@ public class UserRepository : IRepository<User>
         var userToDelete = await GetById(id);
         if (userToDelete == null)
             throw new Exception("User not found");
-        _context.Users.Remove(userToDelete);
-        await SaveChanges();
+        await _userManager.DeleteAsync(userToDelete);
 
     }
 
-    public async Task SaveChanges()
+    public Task SaveChanges()
     {
-        await _context.SaveChangesAsync();
+        throw new System.NotImplementedException("You need to implement this function, UserManager from Identity takes" +
+                                                 "care of it :)");
     }
 }
