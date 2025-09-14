991a3936497989cf064fa5817678cabd0017c452
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-14 20:18:06 +0200

    Add IUserService.cs

---
+++ Message-Backend/Message-Backend/Service/IUserService.cs
@@ -0,0 +1,14 @@
+using Message_Backend.Models;
+
+namespace Message_Backend.Service;
+
+public interface IUserService
+{
+   public Task<User> Get(string id);
+   public Task Add(User user);
+   public Task Update(User user);
+   public Task Delete(string id);
+   public Task<IEnumerable<User>> SearchForUsers(string term);
+   
+   
+}
