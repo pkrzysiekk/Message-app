2d8e2eaa77b9adb118f1e745b1b5f0365f34f6ac
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-14 20:19:33 +0200

    Moc idenity and JWT in Program.cs

--- Message-Backend/Message-Backend/Program.cs
+++ Message-Backend/Message-Backend/Program.cs
@@ -1,5 +1,8 @@
 using Message_Backend.Data;
+using Message_Backend.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +13,40 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContextPool<MessageContext>(opt=>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("MessageDb")));
+builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
+    {
+        // np. konfiguracja haseł
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<MessageContext>()  // <-- powiedz Identity, żeby używał Twojej bazy
+    .AddDefaultTokenProviders();               // <-- potrzebne np. do resetu hasła
+
+// 3. Dodaj autoryzację i uwierzytelnianie (np. JWT, cookies itp.)
+// jeśli JWT:
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var messageContext = scope.ServiceProvider.GetRequiredService<MessageContext>();
-    messageContext.Database.EnsureCreated();
+    messageContext.Database.Migrate();
 }
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,6 +56,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
