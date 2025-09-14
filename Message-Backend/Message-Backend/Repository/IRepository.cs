3681f68b27fd0c8aca9db22cba750b92d21e0217
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-14 18:25:33 +0200

    Add generic repository

---
+++ Message-Backend/Message-Backend/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+namespace Message_Backend.Repository;
+
+public interface IRepository<T> where T : class
+{
+   public Task Create(T item);
+   public Task<IEnumerable<T>> GetAll();
+   public Task<T> GetById(int id);
+   public Task Update(T item);
+   public Task Delete(int id);
+}
