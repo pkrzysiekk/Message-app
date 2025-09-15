2480a48cd4522f0a638d5104e097d05b8956fb1c
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-15 16:56:43 +0200

    Change IUserService.cs

--- Message-Backend/Message-Backend/Service/IUserService.cs
+++ Message-Backend/Message-Backend/Service/IUserService.cs
@@ -5,7 +5,7 @@ namespace Message_Backend.Service;
 public interface IUserService
 {
    public Task<User> Get(string id);
-   public Task Add(User user);
+   public Task Add(User user,string password);
    public Task Update(User user);
    public Task Delete(string id);
    public Task<IEnumerable<User>> SearchForUsers(string term);
