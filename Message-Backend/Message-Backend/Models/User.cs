8769ff8193377d004c1d7ef0ad95c3131d8e52c4
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-15 18:04:15 +0200

    Update User model

--- Message-Backend/Message-Backend/Models/User.cs
+++ Message-Backend/Message-Backend/Models/User.cs
@@ -9,9 +9,9 @@ public class User : IdentityUser<int>
     public int AvatarId { get; set; }
     public DateTime LastSeen { get; set; }
     public bool IsOnline { get; set; }
-
     public ICollection<UserGroup> UserGroups { get; set; } = [];
     public ICollection<Message> Messages { get; set; } = [];
     public ICollection<Friends> Friends { get; set; } = [];
     public Avatar? Avatar { get; set; }
+
 }
