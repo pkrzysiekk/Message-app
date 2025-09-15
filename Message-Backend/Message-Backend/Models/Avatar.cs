93bc872d3a693e906354a6b649bf2af96c654912
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-15 18:04:00 +0200

    Update Avatar model

--- Message-Backend/Message-Backend/Models/Avatar.cs
+++ Message-Backend/Message-Backend/Models/Avatar.cs
@@ -3,7 +3,7 @@ namespace Message_Backend.Models;
 public class Avatar
 {
     public int Id { get; set; }
-    public byte[] content { get; set; }
+    public required byte[] Content { get; set; }
     public int UserId { get; set; }
     
     public User User { get; set; }
