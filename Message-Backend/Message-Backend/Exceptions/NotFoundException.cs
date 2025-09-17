4e896e17a321ecc4b4e50c76aca59dfbe3134822
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-17 20:45:27 +0200

    Add domain-level exceptions

---
+++ Message-Backend/Message-Backend/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace Message_Backend.Exceptions;
+
+public class NotFoundException : Exception
+{
+   public NotFoundException(string message) : base(message)
+   {
+      
+   } 
+}
---
+++ Message-Backend/Message-Backend/Exceptions/UserManagerException.cs
@@ -0,0 +1,9 @@
+namespace Message_Backend.Exceptions;
+
+public class UserManagerException : Exception
+{
+   public UserManagerException(string message) : base(message)
+   {
+      
+   } 
+}
