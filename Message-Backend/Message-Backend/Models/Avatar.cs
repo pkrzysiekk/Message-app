6a0600a288d1b0c060bfe0e3560b2f91ec43df4f
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-18 20:04:07 +0200

    Update Avatar.cs model

--- Message-Backend/Message-Backend/Models/Avatar.cs
+++ Message-Backend/Message-Backend/Models/Avatar.cs
@@ -4,6 +4,7 @@ public class Avatar
 {
     public int Id { get; set; }
     public required byte[] Content { get; set; }
+    public required string ContentType { get; set; }
     public int UserId { get; set; }
     
     public User? User { get; set; }
