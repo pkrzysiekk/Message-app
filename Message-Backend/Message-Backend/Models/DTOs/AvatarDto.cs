0664d18bb7f6d900733ce0cee9b51384f8ee1e6c
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-18 20:20:38 +0200

    Add Content type prop to AvatarDto.cs

--- Message-Backend/Message-Backend/Models/DTOs/AvatarDto.cs
+++ Message-Backend/Message-Backend/Models/DTOs/AvatarDto.cs
@@ -2,6 +2,6 @@ namespace Message_Backend.Models.DTOs;
 
 public class AvatarDto
 {
-    public int UserId { get; set; }
     public required byte[] Content { get; set; }
+    public required string ContentType { get; set; }
 }
