75e811ec538a43494d726d4d868200021faff6f1
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-20 21:13:39 +0200

    Implement GroupRepository.cs

---
+++ Message-Backend/Message-Backend/Repository/GroupRepository.cs
@@ -0,0 +1,54 @@
+using Message_Backend.Data;
+using Message_Backend.Exceptions;
+using Message_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Message_Backend.Repository;
+
+public class GroupRepository : IRepository<Group>
+{
+    private readonly MessageContext _context;
+    public GroupRepository(MessageContext context)
+    {
+        _context = context;
+    }
+    public async Task Create(Group item)
+    {
+       _context.Groups.Add(item); 
+       await SaveChanges();
+    }
+
+    public IQueryable<Group> GetAll()
+    {
+        return _context.Groups.Include(g=>g.UserGroups);
+    }
+
+    public async Task<Group?> GetById(int id)
+    {
+        return await _context.Groups.FindAsync(id);
+    }
+
+    public async Task Update(Group item)
+    {
+        var groupToUpdate= await _context.Groups.FindAsync(item.Id);
+        if (groupToUpdate == null)
+            throw new NotFoundException("Group not found");
+        groupToUpdate.Name = item.Name;
+        groupToUpdate.Type = item.Type;
+        await SaveChanges();
+    }
+
+    public async Task Delete(int id)
+    {
+        var groupToDelete = await _context.Groups.FindAsync(id);
+        if (groupToDelete == null)
+            throw new NotFoundException("Group not found");
+        _context.Groups.Remove(groupToDelete);
+        await SaveChanges();
+    }
+
+    public async Task SaveChanges()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
