57230a85fa9d7ce9ebdb3afe8c5d52c0a0eefa4a
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-21 21:11:32 +0200

    Update IGroupService.cs

--- Message-Backend/Message-Backend/Service/IGroupService.cs
+++ Message-Backend/Message-Backend/Service/IGroupService.cs
@@ -7,7 +7,7 @@ public interface IGroupService
 {
     public Task CreateGroup(Group group);
     public Task UpdateGroup(Group group);
-    public Task DeleteGroup(Group group);
+    public Task DeleteGroup(int id);
     public Task<List<Group>> GetPaginatedUserGroups(int userId,int page, int pageSize);
     public Task<Group> GetGroup(int id);
     public Task AddUserToGroup(int userId, int groupId,GroupRole role);
