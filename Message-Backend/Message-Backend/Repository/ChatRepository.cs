1b80590d0ae209833605835a3dd4b4996e6f0d04
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-22 20:20:17 +0200

    Implement chat repository

---
+++ Message-Backend/Message-Backend/Repository/ChatRepository.cs
@@ -0,0 +1,54 @@
+using Message_Backend.Data;
+using Message_Backend.Exceptions;
+using Message_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Message_Backend.Repository;
+
+public class ChatRepository :IRepository<Chat>
+{
+    private readonly MessageContext _context;
+
+    public ChatRepository(MessageContext context)
+    {
+        _context = context;
+    } 
+    public async Task Create(Chat item)
+    {
+        _context.Chats.Add(item);
+        await SaveChanges();
+    }
+
+    public IQueryable<Chat> GetAll()
+    {
+        return _context.Chats.Include(c=>c.Group);
+    }
+
+    public async Task<Chat?> GetById(int id)
+    {
+        return await GetAll().FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task Update(Chat item)
+    {
+        var chatToUpdate = await GetById(item.Id);
+        if (chatToUpdate == null)
+            throw new NotFoundException("No Chat found with the specified id"); 
+        chatToUpdate.Name = item.Name;
+        _context.Chats.Update(chatToUpdate);
+        await SaveChanges();
+    }
+
+    public async Task Delete(int id)
+    {
+        var chatToDelete = await GetById(id);
+        if (chatToDelete == null)
+            throw new NotFoundException("No Chat found with the specified id");
+        await SaveChanges();
+    }
+
+    public async Task SaveChanges()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
