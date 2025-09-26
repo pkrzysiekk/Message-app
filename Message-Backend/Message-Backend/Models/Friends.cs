b1101db10281fce05c3e197f6e25977b58661584
pkrzysiek <pkrzysiek24@gmail.com>
2025-09-26 14:19:04 +0200

    Set FriendsSince prop of Friends.cs to be nullable

--- Message-Backend/Message-Backend/Models/Friends.cs
+++ Message-Backend/Message-Backend/Models/Friends.cs
@@ -7,7 +7,7 @@ public class Friends
     public int UserId { get; set; }
     public int FriendId { get; set; }
     public FriendInvitationStatus Status { get; set; }
-    public DateTime FriendsSince { get; set; }
+    public DateTime? FriendsSince { get; set; }
     
     public User User { get; set; }
     public User Friend { get; set; }
