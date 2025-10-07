9aee915d38704859768e8cf0da3a7c9e209c5cb1
pkrzysiek <pkrzysiek24@gmail.com>
2025-10-07 21:00:36 +0200

    Modify AuthController.cs for RSA

--- Message-Backend/Message-Backend/Controllers/AuthController.cs
+++ Message-Backend/Message-Backend/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
+using Message_Backend.Helpers;
 using Message_Backend.Mappers;
 using Message_Backend.Models;
 using Message_Backend.Models.DTOs;
+using Message_Backend.Models.RSA;
 using Message_Backend.Service;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,16 +11,18 @@ namespace Message_Backend.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
+        private readonly JwtOptions _jwtOptions;
         public AuthController(IAuthService authService)
         {
             _authService = authService;
+            _jwtOptions=RsaHelper.LoadJwtOptions();
         }
         
         [HttpPost("/auth")]
         public ActionResult<string> Post([FromBody] UserDto userDto)
         {
             var user = userDto.ToBo();
-            var token = _authService.GenerateToken(user);
+            var token = _authService.GenerateToken(_jwtOptions,user);
             return Ok(token);
         }
     }
