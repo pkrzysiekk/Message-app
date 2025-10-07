eea0c387606468206243bb2a89734af456d6413c
pkrzysiek <pkrzysiek24@gmail.com>
2025-10-07 21:00:50 +0200

    Modify AuthService.cs for RSA

--- Message-Backend/Message-Backend/Service/AuthService.cs
+++ Message-Backend/Message-Backend/Service/AuthService.cs
@@ -1,25 +1,31 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using Message_Backend.Helpers;
 using Message_Backend.Models;
+using Message_Backend.Models.RSA;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Message_Backend.Service;
 
 public class AuthService: IAuthService
 {
-    public string GenerateToken(User user)
+    public string GenerateToken(JwtOptions jwtOptions,User user)
     {
         var handler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes("dsfdsjdsfjsdfjsdfjsdfjsdjfsdjfsdjfjsdjfsdjfjsdfjsdfsdfsd");
+        var rsa=RsaHelper.LoadRsaKey(jwtOptions.PrivateKeyLocation);
+        
         var credentials = new SigningCredentials(
-            new SymmetricSecurityKey(key),
-            SecurityAlgorithms.HmacSha256Signature
+            new RsaSecurityKey(rsa),
+            SecurityAlgorithms.RsaSha256
         );
         var tokenDescriptor = new SecurityTokenDescriptor()
         {
             Subject = GenerateClaims(user),
             Expires = DateTime.Now.AddHours(1),
+            Issuer = jwtOptions.Issuer,
+            Audience = jwtOptions.Audience,
             SigningCredentials = credentials
         };
         var token = handler.CreateToken(tokenDescriptor);
@@ -33,4 +39,5 @@ public class AuthService: IAuthService
         claims.AddClaim(new Claim(ClaimTypes.Email,user.Email));
         return claims;
     }
+
 }
--- Message-Backend/Message-Backend/Service/IAuthService.cs
+++ Message-Backend/Message-Backend/Service/IAuthService.cs
@@ -1,8 +1,9 @@
 using Message_Backend.Models;
+using Message_Backend.Models.RSA;
 
 namespace Message_Backend.Service;
 
 public interface IAuthService
 {
-    public string GenerateToken(User user);
+    public string GenerateToken(JwtOptions jwtOptions,User user);
 }
