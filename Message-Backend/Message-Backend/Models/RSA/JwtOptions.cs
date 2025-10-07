6930852af147b4e337c5e205750c3e283681a10a
pkrzysiek <pkrzysiek24@gmail.com>
2025-10-07 20:59:53 +0200

    Add Jwt options class

---
+++ Message-Backend/Message-Backend/Models/RSA/JwtOptions.cs
@@ -0,0 +1,9 @@
+namespace Message_Backend.Models.RSA;
+
+public class JwtOptions
+{
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string PublicKeyLocation { get; set; }
+    public string PrivateKeyLocation { get; set; }
+}
