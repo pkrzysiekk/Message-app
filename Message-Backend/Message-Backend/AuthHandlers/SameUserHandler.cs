b33a58f5ae25f3d14edfa52e79d648daf3e523cc
pkrzysiek <pkrzysiek24@gmail.com>
2025-10-08 21:41:15 +0200

    Implement Handler for SameUserRequirement

---
+++ Message-Backend/Message-Backend/AuthHandlers/SameUserHandler.cs
@@ -0,0 +1,30 @@
+using System.Security.Claims;
+using Message_Backend.AuthRequirements;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Message_Backend.AuthHandlers;
+
+public class SameUserHandler :AuthorizationHandler<SameUserRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, SameUserRequirement requirement)
+    {
+       var userIdFromToken= context.User.FindFirstValue(ClaimTypes.NameIdentifier); 
+       
+       if (userIdFromToken == null)
+           context.Fail();
+       
+       if (context.Resource is not HttpContext httpContext)
+       {
+           context.Fail();
+           return Task.CompletedTask; 
+       }
+
+       string userIdFromEndpoint;
+       userIdFromEndpoint = httpContext.Request.Query["userId"].ToString();
+       if (string.IsNullOrEmpty(userIdFromEndpoint))
+        httpContext.GetRouteValue("userId").ToString();
+       if (userIdFromEndpoint == userIdFromToken)
+        context.Succeed(requirement);
+       return Task.CompletedTask;
+    }
+}
