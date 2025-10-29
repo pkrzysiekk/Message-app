5cda83e598c05ebb6dbaf85574f9dcc956f5b896
pkrzysiek <pkrzysiek24@gmail.com>
2025-10-29 13:00:30 +0100

    Ovveride OnConnectedAsync Hub method

--- Message-Backend/Message-Backend/Hubs/ChatHub.cs
+++ Message-Backend/Message-Backend/Hubs/ChatHub.cs
@@ -10,10 +10,28 @@ namespace Message_Backend.Hubs;
 public class ChatHub :Hub<IChatClient>
 {
     private readonly IMessageService _messageService;
+    private readonly IChatService _chatService;
    
-    public ChatHub(IMessageService messageService)
+    public ChatHub
+        (IMessageService messageService, IChatService chatService)
     {
         _messageService = messageService;
+        _chatService = chatService;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var callersId = Context.User.Identity.Name;
+        if (callersId is null)
+            return;
+        
+        var userId = Int32.Parse(callersId);
+        var userChats = await _chatService.GetUserChats(userId);
+        foreach (var userChat in userChats)
+        {
+             await Groups.AddToGroupAsync(Context.ConnectionId, userChat.Id.ToString());
+        }
+        await base.OnConnectedAsync();
     }
     
     public async Task SendMessageInChat(MessageDto message)
