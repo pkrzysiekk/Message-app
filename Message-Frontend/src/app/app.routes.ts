85bf5d552350962d7a5c43a0de26afff02376d39
pkrzysiekk <pkrzysiek24@gmail.com>
2025-11-29 19:00:24 +0100

    Add basic auth feature structure with routing

--- Message-Frontend/src/app/app.routes.ts
+++ Message-Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
-export const routes: Routes = [{ path: '', component: App }];
+import path from 'path';
+import { authRoutes } from './features/auth/auth.routes';
+export const routes: Routes = [
+  { path: '', component: App },
+  { path: 'auth', children: authRoutes },
+];
---
+++ Message-Frontend/src/app/features/auth/auth.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import path from 'path';
+import { AuthLayout } from './auth-layout/auth-layout';
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: Login },
+    ],
+  },
+];
