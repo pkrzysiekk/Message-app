48a5958b95d2aa966a66d1b4a666c48ca4516080
pkrzysiekk <pkrzysiek24@gmail.com>
2025-12-01 13:58:44 +0100

    Add routing to register component from login screen

--- Message-Frontend/src/app/features/auth/auth.routes.ts
+++ Message-Frontend/src/app/features/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import path from 'path';
 import { AuthLayout } from './auth-layout/auth-layout';
+import { Register } from './register/register';
 
 export const authRoutes: Routes = [
   {
@@ -10,6 +11,7 @@ export const authRoutes: Routes = [
     children: [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: Login },
+      { path: 'register', component: Register },
     ],
   },
 ];
