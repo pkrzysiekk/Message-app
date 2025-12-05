5f806a8930f7edbec88aa81663e27d46e4cb7d31
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-05 12:07:45 +0100

    Add auth interceptor

--- Message-Frontend/src/app/app.config.ts
+++ Message-Frontend/src/app/app.config.ts
@@ -5,12 +5,13 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { httpErrorInterceptor } from './core/interceptors/error-interceptor/http-error-interceptor';
+import { authInterceptor } from './core/interceptors/auth-interceptor/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor, authInterceptor])),
   ],
 };
--- Message-Frontend/src/app/core/interceptors/auth-interceptor/auth-interceptor.ts
+++ Message-Frontend/src/app/core/interceptors/auth-interceptor/auth-interceptor.ts
@@ -1,5 +1,18 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (!authService.isUserAuthenticated()) {
+    router.navigate(['/auth/login']);
+    return throwError(() => new HttpErrorResponse({ status: 401 }));
+  }
+  const newReq = req.clone({
+    headers: req.headers.append('Authorization', `Bearer ${authService.authUser().authToken}`),
+  });
+  return next(newReq);
 };
