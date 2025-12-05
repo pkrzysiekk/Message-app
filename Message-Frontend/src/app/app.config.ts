e0ec956afbb6c5b383a8c8423928057e4fa728e3
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-05 11:23:44 +0100

    Change interceptors folder structure

--- Message-Frontend/src/app/app.config.ts
+++ Message-Frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { httpErrorInterceptor } from './core/interceptors/http-error-interceptor';
+import { httpErrorInterceptor } from './core/interceptors/error-interceptor/http-error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
