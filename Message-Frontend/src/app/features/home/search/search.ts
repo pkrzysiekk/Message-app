d5b48c41a4130a94cfdaf1e16437d27047314c85
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-06 21:36:58 +0100

    Add search functionality

--- Message-Frontend/src/app/features/home/search/search.ts
+++ Message-Frontend/src/app/features/home/search/search.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, model, signal } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Field, form } from '@angular/forms/signals';
+import { debounceTime } from 'rxjs';
+import { User } from '../../../core/services/auth/models/user';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
-
+  http = inject(HttpClient);
+  searchTerm = '';
+  fetchedUsers = signal<User[]>([]);
+  onSearch = () => {
+    if (!this.searchTerm) return;
+    const reqUrl = `https://localhost/api/user?term=${this.searchTerm}`;
+    this.http
+      .get<User[]>(reqUrl)
+      .pipe(debounceTime(1000))
+      .subscribe((resp) => this.fetchedUsers.set(resp));
+  };
 }
