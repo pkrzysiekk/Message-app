fe017bc72f87b1f7c445e238816f7e858a30f46b
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-12 20:51:04 +0100

    Add mock navbar

--- Message-Frontend/src/app/features/home/home/home.ts
+++ Message-Frontend/src/app/features/home/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, signal, Signal } from '@angular/core';
 import { Search } from '../../user/search/search';
+import { Navbar } from '../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-home',
-  imports: [Search],
+  imports: [Search, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
