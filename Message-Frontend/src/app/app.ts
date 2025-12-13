f4c55e59b1bd823c7403c6df47c18a11d25d07ed
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-13 23:06:24 +0100

    Try fetching the data about the current user on App's init

--- Message-Frontend/src/app/app.ts
+++ Message-Frontend/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './core/services/user/user-service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Message-Frontend');
+  protected readonly userService = inject(UserService);
+  ngOnInit() {
+    this.userService.setLocalUser();
+  }
 }
