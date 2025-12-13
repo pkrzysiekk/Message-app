d7b244ee85ba7019a188dd675e38b9b86451e448
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-13 23:04:46 +0100

    Add functionality in userService to fetch currently logged in user's data

--- Message-Frontend/src/app/core/services/user/user-service.ts
+++ Message-Frontend/src/app/core/services/user/user-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../../models/user';
+import { get } from 'http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,22 @@ import { User } from '../../models/user';
 export class UserService {
   userApiUrl = 'https://localhost/api/user';
   http = inject(HttpClient);
+  localUser = signal<User | null>(null);
+
+  private getLocalUserData = () => {
+    this.http.get<User>(`${this.userApiUrl}/me`).subscribe({
+      next: (user) => {
+        this.localUser.set(user);
+      },
+      error: () => this.localUser.set(null),
+    });
+  };
+
+  setLocalUser() {
+    if (this.localUser()) return;
+    this.getLocalUserData();
+  }
+
   getUser(id: number) {
     return this.http.get<User>(`${this.userApiUrl}/${id}`);
   }
