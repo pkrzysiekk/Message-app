d3480a7b8db74b3a280a83c528fac42e6e6005dc
pkrzysiek <pkrzysiek24@gmail.com>
2025-12-19 14:23:09 +0100

    Update http-error-interceptor to redirect to log in screen on 401 error code

--- Message-Frontend/src/app/core/interceptors/error-interceptor/http-error-interceptor.ts
+++ Message-Frontend/src/app/core/interceptors/error-interceptor/http-error-interceptor.ts
@@ -1,10 +1,13 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      console.log('log', error);
+      if (error.status == 401) router.navigate(['/auth']);
       return throwError(() => error);
     }),
   );
