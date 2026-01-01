deafac96ad8fa494a966fe67d9327f17dc86957e
pkrzysiek <pkrzysiek24@gmail.com>
2026-01-01 13:56:43 +0100

    Save changes before friends module refactor

--- Message-Frontend/src/app/shared/pipes/image-parse-pipe/image-parse-pipe.ts
+++ Message-Frontend/src/app/shared/pipes/image-parse-pipe/image-parse-pipe.ts
@@ -1,6 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Image } from '../../../core/models/image';
-
 @Pipe({
   name: 'imageParse',
 })
@@ -8,7 +7,8 @@ export class ImageParsePipe implements PipeTransform {
   transform(image?: Image): string | null {
     const srcPrefix = 'data:';
     const srcSuffix = ';base64,';
-    if (!image) return null;
+    const placeholderImage = 'pic_placeholder.png';
+    if (!image) return placeholderImage;
     return srcPrefix + image.contentType + srcSuffix + image.content;
   }
 }
