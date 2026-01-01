c62580f3ea80db4e5d030509d96cc7f4e113b2d9
pkrzysiek <pkrzysiek24@gmail.com>
2026-01-01 14:31:53 +0100

    Refactor friends' component to user newer API method

--- Message-Frontend/src/app/features/friends/friends/friends.ts
+++ Message-Frontend/src/app/features/friends/friends/friends.ts
@@ -18,20 +18,15 @@ export class Friends {
   private friendsService = inject(FriendsService);
   private userService = inject(UserService);
   userId = this.userService.localUser()?.id.toString();
-  private fetchedInvites = signal<FriendsInvitation[] | null>(null);
-  private friendIds = signal<string[]>([]);
   private inviterIds = signal<string[]>([]);
   friends = signal<User[]>([]);
   inviters = signal<User[]>([]);
   showInvites = signal<boolean>(true);
 
   fetchFriends() {
-    this.friendsService.getFriends().subscribe({
-      next: (friends) => {
-        const ids = friends.map((friend) =>
-          friend.userId === this.userId ? friend.friendId : friend.userId!,
-        );
-        this.friendIds.set(ids);
+    this.friendsService.getUsersFromFriends().subscribe({
+      next: (fetched) => {
+        this.friends.set(fetched);
       },
     });
   }
@@ -51,34 +46,6 @@ export class Friends {
   constructor() {
     this.fetchFriends();
     this.fetchInvites();
-
-    effect((onCleanup) => {
-      const ids = this.friendIds();
-      console.log(ids);
-      if (!ids.length) {
-        this.friends.set([]);
-        return;
-      }
-      const sub = forkJoin(ids.map((id) => this.userService.getUser(+id))).subscribe({
-        next: (friends) => {
-          this.friends.set(friends);
-        },
-      });
-
-      onCleanup(() => sub.unsubscribe());
-    });
-
-    effect((onCleanup) => {
-      const ids = this.inviterIds();
-      if (!ids.length) this.inviters.set([]);
-      const sub = forkJoin(ids.map((id) => this.userService.getUser(+id))).subscribe({
-        next: (inv) => {
-          this.inviters.set(inv);
-          console.log('inv', inv);
-        },
-      });
-      onCleanup(() => sub.unsubscribe());
-    });
   }
   onAccept(friendId: number) {
     this.friendsService.acceptInvite(friendId).subscribe(() => {
